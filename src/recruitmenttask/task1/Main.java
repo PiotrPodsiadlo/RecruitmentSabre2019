a302bc8a8c684589c0f4abb8a30b56bcc4a2f7e7
Piotr <piotrekchorzow@interia.eu>
2019-08-08 15:56:17 +0200

    fixed formatting

--- src/recruitmenttask/task1/Main.java
+++ src/recruitmenttask/task1/Main.java
@@ -1,7 +1,5 @@
 package recruitmenttask.task1;
-
 import recruitmenttask.task1.service.InputText;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,13 +7,10 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        String string = InputText.textFromconsole("please enter text(any non-letter characters and polish characters will be ignored!!!)").trim();
+        String string = InputText.textFromconsole("please enter text(any non-letter characters will be ignored!!!)").trim();
         Set<String> lettersInText = new HashSet<>(Arrays.asList(string.replaceAll("\\s", "").split("")));
         Set<String> nonrepeatingWordsInText = new HashSet<>(Arrays.asList(string.split(" ")));
-
-
-        for (String s: lettersInText
-             ) {
+        for (String s: lettersInText) {
             System.out.print(s + ": ");
             nonrepeatingWordsInText.stream().sorted().filter(elem -> elem.contains(s)).forEach(elem -> System.out.print(elem + ", "));
             System.out.println();
--- src/recruitmenttask/task1/service/InputText.java
+++ src/recruitmenttask/task1/service/InputText.java
@@ -1,23 +1,16 @@
 package recruitmenttask.task1.service;
-
 import java.util.Scanner;
-
 public class InputText {
-
-
 
 public static String textFromconsole(String prompt) {
 
     Scanner scan = new Scanner(System.in);
     System.out.println(prompt);
     String name = scan.nextLine();
-
     while(name.trim().length()==0 ){
-
         System.out.println("no text detected, enter again");
         name = scan.nextLine();
     }
-
-    return name.trim().replaceAll("[^a-zA-Z\\s ]", "");
+    return name.trim().replaceAll("[^a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ\\s ]", "");
     }
 }
